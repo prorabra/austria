4bce9a6efa4fec823c86b5fe582ef7c577092dcb
Andrey <zai4enko.andrey@gmail.com>
2018-07-24 14:56:23 +0300

    add all pages and modificate mobile version

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+    var $dropdowns = $('.dropdown');
+$dropdowns.click(function() {
+  if ( $(this).hasClass('active') ){
+    $(this).toggleClass('active');
+  } else {
+    $dropdowns.removeClass('active');
+    $(this).toggleClass('active');
+  }
+});
